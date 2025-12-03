c142e7cde7d2a333a461435e568cddbc2752de6f
yeonwoo0981 <0981syw@naver.com>
2025-12-03 18:32:20 +0900

    12/3
    에셋 도배하고 게임 꾸미기 먼저 해버리기

---
+++ Assets/01_Scripts/Agent/DashCompo.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Agent
+{
+    public class DashCompo : MonoBehaviour
+    {
+        [SerializeField] private float dashPower = 5f;
+        [SerializeField] private float dashCooldown = 5f;
+        private bool _isCoolTime = false;
+        private Rigidbody2D _rb;
+        private bool _doDash = false;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            if (!_isCoolTime && Input.GetKeyDown(KeyCode.Space))
+            {
+                _doDash = true;
+                _isCoolTime = true;
+                StartCoroutine(CoolDown());
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            if (_doDash)
+            {
+                Debug.Log("Dash requested");
+                _doDash = false;
+                _rb.AddForce(Vector2.up * dashPower, ForceMode2D.Impulse);
+            }
+        }
+
+        private IEnumerator CoolDown()
+        {
+            yield return new WaitForSeconds(dashCooldown);
+            _isCoolTime = false;
+        }
+    }
+}
