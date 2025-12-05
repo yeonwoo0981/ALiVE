4e109a3965b0c877129dd82ba45aea729a0fca40
yeonwoo0981 <0981syw@naver.com>
2025-12-05 20:54:14 +0900

    12/5 Final

---
+++ Assets/01_Scripts/UI/DeathCinema.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _01_Scripts.UI
+{
+    public class DeathCinema : MonoBehaviour
+    {
+        private Image _image;
+
+        private void Awake()
+        {
+            _image = GetComponent<Image>();
+        }
+    }
+}
---
+++ Assets/01_Scripts/UI/PlayerDeathCinema.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using _01_Scripts.System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _01_Scripts.UI
+{
+    public class PlayerDeathCinema : MonoBehaviour
+    {
+        private HealthSystem _healthSystem;
+        [SerializeField] DeathCinema deathCinemaImage;
+        
+        private void Awake()
+        {
+            _healthSystem = GetComponent<HealthSystem>();
+            deathCinemaImage.gameObject.SetActive(false);
+        }
+
+        private void OnEnable()
+        {
+            _healthSystem.OnDead += DeathCinema;
+        }
+
+        private void DeathCinema()
+        {
+            deathCinemaImage.gameObject.SetActive(true);
+        }
+        
+        private void OnDisable()
+        {
+            _healthSystem.OnDead += DeathCinema;
+        }
+    }
+}
